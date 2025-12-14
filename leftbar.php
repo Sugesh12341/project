cd2ca723759f18b74c1482d0a2e7d49945096b9b
Sugesh12341 <ssddaaqff123@gmail.com>
2025-12-13 21:24:24 -0800

    Add files via upload

---
+++ leftbar.php
@@ -0,0 +1,47 @@
+<style>
+   .pay{
+    background-color: red;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: 900;
+    padding-left: 20px;
+   } 
+   .pay:hover,.pay:focus{
+    background-color:blue;
+    
+   }
+</style>
+<nav class="ts-sidebar">
+    <ul class="ts-sidebar-menu">
+        <li class="ts-label">Main</li>
+        <div class="pay">
+        <li><a href="inde.php" title="Manage website pages"><i class="fa fa-files-o"></i> 
+         Invoice </a></li></div>
+        <li><a href="dashboard.php" title="Dashboard"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+
+        <li>
+            <a href="#"><i class="fa fa-files-o"></i> Brands</a>
+            <ul>
+                <li><a href="create-brand.php" title="Create a new brand">Create Brand</a></li>
+                <li><a href="manage-brands.php" title="Manage existing brands">Manage Brands</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="#"><i class="fa fa-sitemap"></i> Bikes</a>
+            <ul>
+                <li><a href="post-avehical.php" title="Post a new vehicle">Post a Bike</a></li>
+                <li><a href="manage-vehicles.php" title="Manage vehicle listings">Manage Bikes</a></li>
+            </ul>
+        </li>
+
+        <li><a href="manage-bookings.php" title="Manage customer bookings"><i class="fa fa-users"></i> Manage Booking</a></li>
+        <li><a href="testimonials.php" title="Manage customer testimonials"><i class="fa fa-table"></i> Manage Testimonials</a></li>
+        <li><a href="manage-conactusquery.php" title="Manage customer inquiries"><i class="fa fa-desktop"></i> Manage Contact Us Queries</a></li>
+        <li><a href="reg-users.php" title="View registered users"><i class="fa fa-users"></i> Registered Users</a></li>
+        <li><a href="manage-pages.php" title="Manage website pages"><i class="fa fa-files-o"></i> Manage Pages</a></li>
+        
+        <li><a href="update-contactinfo.php" title="Update website contact details"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
+        <li><a href="manage-subscribers.php" title="Manage email subscribers"><i class="fa fa-table"></i> Manage Subscribers</a></li>
+    </ul>
+</nav>
